338be76de8210e79f8ce310d3cbf17211f1b5baf
Katie Moosman <moosman.katie@gmail.com>
2018-12-16 19:41:40 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -36,7 +36,7 @@ class Song
   
   def self.find_or_create_by_name(name)
     if @@all.include?(name) 
-        @@all.find {|song| song.name == name return song}
+        @@all.find {|song| song.name == name puts song}
     else 
       song = self.new
       song.name = name
